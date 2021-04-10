9c0fe1a55834bc9ed8ddc711b3eecefc7f09ec09
clairewkh <72780095+clairewkh@users.noreply.github.com>
2021-04-10 04:43:28 -0400

    Adding Gauge Chart (Missing bubble chart)

--- static/js/app.js
+++ static/js/app.js
@@ -24,7 +24,6 @@ d3.json(data_url).then(function(read_data){
             var fil_data = data1[0].samples.filter(item => item.id === select_id_names)
             console.log(fil_data)
 
-            //////////////////////////////////////
 
             var top10_otu = []
             fil_data[0].otu_ids.slice(0,10).forEach(i => 
@@ -37,6 +36,7 @@ d3.json(data_url).then(function(read_data){
         
             console.log(top10_otu)
 
+            //Bar Chart
             var bar_data = 
             [{
                 type: 'bar',
@@ -61,7 +61,7 @@ d3.json(data_url).then(function(read_data){
 
             Plotly.newPlot('bar', bar_data, bar_layout);
 
-            
+            //Demographics Info
             var fil_meta = data1[0].metadata.filter(item=>item.id === parseInt(select_id_names));
             console.log(fil_meta)
 
@@ -76,12 +76,51 @@ d3.json(data_url).then(function(read_data){
             demoMetadata.append('p').text(`BBtype: ${fil_meta[0].bbtype}`);
             demoMetadata.append('p').text(`Wfreq: ${fil_meta[0].wfreq}`);
 
+            //Gauge-Bonus
+            
+            var data = [
+                {
+                    domain: { x: [0, 1], y: [0, 1] },
+                    type: 'indicator',
+                    mode: 'gauge+number',
+                    value: fil_meta[0].wfreq,
+                    title: {
+                        text: 'Belly Button Washing Frequency <br> Scrubs per Week</i>',
+                        font: { size: 26, color: 'Green'}
+                    },
+                    gauge: {
+                        axis: { range: [null, 9] },
+                        bar: { color: '#FFFACD', thickness: 0.4 },
+                        bordercolor: 'gray',
+            
+                        steps: [
+                            { range: [0, 1], color: '#D8BFD8' },
+                            { range: [1, 2], color: '#DDA0DD' },
+                            { range: [2, 3], color: '#DA70D6' },
+                            { range: [3, 4], color: '#FF00FF'},
+                            { range: [4, 5], color: '#BA55D3' },
+                            { range: [5, 6], color: '#9932CC' },
+                            { range: [6, 7], color: '#9500D3' },
+                            { range: [7, 8], color: '#8B008B' },
+                            { range: [8, 9], color: '#800080' }
+                        ],
+                    },
+                },
+            ];
+            // Layout 
+            var layout = { width: 600, height: 450, margin: { t: 0, b: 0 } };
+            // Render the plot to the div tag with id 'gauge'
+            Plotly.newPlot('gauge', data, layout);
+                }
+            
+            
+            
 
 
 
 
 
-        }
+        })
 
 
 
@@ -95,7 +134,6 @@ d3.json(data_url).then(function(read_data){
 
 
 
-    })
 
 
 
